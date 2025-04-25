8aa32166b963766e281a673676efe55a9ec37fe6
Salah_Eddine_Saadani <salaheddinesaadani@gmail.com>
2025-04-25 16:40:42 +0200

    Add files via upload

---
+++ CORSE 8 - DATE 1 LESS THAN DATE 2/CORSE 8 - PROBLEM 13 - DATE 1 LESS THAN DATE 2/CORSE 8 - PROBLEM 13 - DATE 1 LESS THAN DATE 2.cpp
@@ -0,0 +1,69 @@
+// CORSE 8 - PROBLEM 13 - DATE 1 LESS THAN DATE 2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+struct stDate
+{
+    short Year;
+    short Month;
+    short Day;
+};
+bool IsDate1BeforeDate2(stDate Date1, stDate Date2)
+{
+    return (Date1.Year < Date2.Year) ? true : ((Date1.Year == Date2.Year) ? (Date1.Month < Date2.Month ? true : (Date1.Month == Date2.Month ? Date1.Day < Date2.Day : false)) : false);
+}
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease Enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+short ReadMonth()
+{
+    short Month;
+    cout << "Please Enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+short ReadYear()
+{
+    short Year;
+    cout << "Please Enter a Year? ";
+    cin >> Year;
+    return Year;
+}
+stDate ReadFullDate()
+{
+    stDate Date;
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+int main()
+{
+    stDate Date1 = ReadFullDate();
+    stDate Date2 = ReadFullDate();
+
+    if (IsDate1BeforeDate2(Date1, Date2))
+        cout << "\nYes, Date1 is Less than Date2.";
+    else
+        cout << "\nNo, Date1 is Not Less than Date2.";
+    system("pause>0");
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
